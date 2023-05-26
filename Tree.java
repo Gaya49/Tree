b0643af30964ed5817d65f87ee9ece2e35d29cf6
Gaya49 <134685596+Gaya49@users.noreply.github.com>
2023-05-26 10:02:35 +0200

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -5,11 +5,6 @@
 // LEAF: contiene il solo albero vuoto "leaf"
 // BRANCH: contiene tutti gli alberi non vuoti
 
-// La classe astratta contiene il nome della classe, "Tree", e il
-// minimo di metodi che richiediamo per formare una classe concreta
-// di alberi.
-// In questa implementazione quando inseriamo cancelliamo: dunque
-// il contenuto precedente di un albero NON e' ricostruibile
 
 public abstract class Tree {
     //Test se l'albero e' vuoto
@@ -39,8 +34,7 @@ public abstract class Tree {
     public String toString() {
         return toStringAux("", "___", "   ", "   ");
     }
-  /* Trascrizione albero --> stringa. Ogni albero viene trascritto in
-stringa dall’alto verso il basso, un elemento per riga, con i sottoalberi disegnati piu' a destra dell’albero di cui fan parte. Il risultato e’ un disegno bidimensionale fatto con soli caratteri ascii. */
+  
 }
-// end class Tree
+
 
